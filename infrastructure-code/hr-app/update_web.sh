fc4a76b1c7cd9b746d4c6f121f81484cbd056899
Mohammad Badiyu Zama <zamamb@gmail.com>
2017-07-11 21:07:58 +0530

    Update update_web.sh

--- infrastructure-code/hr-app/update_web.sh
+++ infrastructure-code/hr-app/update_web.sh
@@ -7,8 +7,8 @@ docker rmi --force hr-web-image:v1
 
 echo 'Building Image with latest changes ...'
 docker build -t hr-web-image:v1 .
-docker run -d -p 9090:8080 --name hr-web-container hr-web-image:v1
+docker run -d -p 9091:8080 --name hr-web-container hr-web-image:v1
 cd ..
 
 echo 'Access the Web application using this url:'
-echo 'http://192.168.99.100:9090/hr-web'
+echo 'http://192.168.99.100:9091/hr-web'
