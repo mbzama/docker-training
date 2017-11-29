7fc1faf279658d58c051abf884b99aa7e0e0df67
Mohammad Badiyu Zama <zamamb@gmail.com>
2017-11-29 12:37:38 +0530

    Updated

--- source-code/event-processor/src/main/java/zama/learning/event/processor/EventProcessorDemo.java
+++ source-code/event-processor/src/main/java/zama/learning/event/processor/EventProcessorDemo.java
@@ -4,9 +4,10 @@ public class EventProcessorDemo {
 	private static Integer counter = 0;
 	
 	public static void main(String[] args) throws InterruptedException {
+		String javaVersion = "Running on "+System.getProperty("java.version")+" java version - ";
 		
 		while(true){
-			System.out.println("Processing Event: "+counter);
+			System.out.println(javaVersion + new Date() +" - Processing Event: "+counter );
 			counter++;
 			Thread.sleep(3000);
 		}
